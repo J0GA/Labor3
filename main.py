5e4740d2dcd503599403290346f8c76f1fd6cb2f
Stas <SLn00200182@gmail.com>
2022-04-14 10:19:16 +0300

    Finished

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def prefix(s):
             j += 1
     return P
 
-def kmp(sub:str, s:str):
+def kmp(sub, s):
     k = 0
     l = 0
     P = prefix(sub)
@@ -36,25 +36,154 @@ def kmp(sub:str, s:str):
     return -1
 
 
-s1 = input()
-sub1 = input()
+def displacement(subStr):
+    table = [len(subStr)] * 256
+    for i in range(len(subStr) - 1):
+        table[ord(subStr[i])] = len(subStr) - 1 - i
+    return table
+
+
+# упрощённый алгоритм Бойера-Мура
+def bm_search(subStr, line, case, space):
+    space = space.lower()
+    case = case.lower()
+    _subStr = subStr
+    _line = line
+    if case == "нет" or "Нет":  # если пользователь хочет найти подстроку, неважно в каком регистре строка и подстрока
+        _subStr = _subStr.lower()
+        _line = _line.lower()
+    if space == "нет" or "Нет":  # если пользователь не хочет обращать внимания на пробелы и их количество
+        _subStr = _subStr.replace(" ", "")
+        _line = _line.replace(" ", "")
+
+    table = displacement(_subStr)
+    i = len(_subStr) - 1
+    j = i
+    k = i
+    while j >= 0 and i <= len(_line) - 1:
+        j = len(_subStr) - 1
+        k = i
+        while j >= 0 and _line[k] == _subStr[j]:
+            k -= 1
+            j -= 1
+        i += table[ord(_line[i])]
+
+    if k >= len(_line) - len(_subStr):
+        return -1
+    else:
+        if space == "no":
+            m = 0
+            n = 0
+            spacesNumber = 0
+            while m <= k + 1 and n <= len(line):
+                if _line[m] == line[n]:
+                    m += 1
+                    n += 1
+                else:
+                    if line[n] == " ":
+                        spacesNumber += 1
+                        n += 1
+            return k + 1 + spacesNumber
+        else:
+            return k + 1
+
+print("Алгоритм Кнута-Морриса-Пратта")
+s1 = input("Введите строку: ")
+sub1 = input("Введите подстроку: ")
 lsub1 = len(sub1)
 start_time=timeit.default_timer()
 index = kmp(sub1, s1)
 if index != -1:
-    print(f'Подстрока "{s1[index:index+lsub1]}"найдена под индексом {index}')
+    print(f'Подстрока "{sub1}" найдена под индексом {index}')
 else:
     print("Данной подстроки не существует")
 print(f"Алгоритм Кнута-Морриса-Пратта выполнил работу за {str(timeit.default_timer() - start_time)} сек")
 print()
 
-s2 = input()
-sub2 = input()
-lsub2 = len(sub1)
-start_time=timeit.default_timer()
-index = kmp(sub2, s2)
-if index != -1:
-    print(f'Подстрока "{s1[index:index+lsub1]}"найдена под индексом {index}')
-else:
-    print("Данной подстроки не существует")
-print(f"Алгоритм Кнута-Морриса-Пратта выполнил работу за {str(timeit.default_timer() - start_time)} сек")
+print("Упрощенный алгоритм Бойера-Мура")
+s2 = input("Введите строку: ")
+sub2 = input("Введите подстроку: ")
+case2 = input("Чувствительность к регистру, напишите да или нет: ")
+space2 = input("Чувствительность к пробелам, напишите да или нет: ")
+startTime = timeit.default_timer()
+k = bm_search(sub2, s2, case2, space2)
+print(f'Подстрока "{sub2}" найдена под индексом {k}')
+print(f"Упрощенный алгоритм Бойера-Мура выполнил работу за {str(timeit.default_timer() - startTime)} сек")
+
+from queue import PriorityQueue
+
+
+class Position:
+    def __init__(self, position, start_distance, finish_distance):
+        self.position = position
+        self.start_distance = start_distance
+        self.finish_distance = finish_distance
+    def __str__(self):
+        return '\n'.join((N * '{:3}').format(*[i % (N * N) for i in self.position[i:]]) for i in range(0, N * N, N))
+    def __lt__(self, other):
+        return self.start_distance + self.finish_distance < other.start_distance + other.finish_distance
+
+N = 4
+def shifts(position):
+    zeroPosition = position.index(0)
+    i, j = divmod(zeroPosition, N)
+    displacement = []
+    if i > 0: displacement.append(-N)  # вверх
+    if i < N - 1: displacement.append(N)  # вниз
+    if j > 0: displacement.append(-1)  # влево
+    if j < N - 1: displacement.append(1)  # вправо
+    for offset in displacement:
+        swap = zeroPosition + offset
+        yield tuple(
+            position[swap] if x == zeroPosition else position[zeroPosition] if x == swap else position[x] for x in
+            range(N * N))
+
+
+def parityOfPairs(state):
+    countOfPairs = 0
+    for i in range(len(state) - 1):
+        if state[i] > state[i + 1]:
+            countOfPairs += 1
+    return countOfPairs % 2
+
+
+def fifteenGame(startState):
+    terminalState = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0)
+    if parityOfPairs(startState) == 0:
+        print("Нет решений")
+    else:
+        startState = tuple(startState)
+        p = Position(startState, 0, 0)
+        print(p)
+        print()
+        fieldStates = PriorityQueue()
+        fieldStates.put(p)
+        closePoints = set([p])
+        parents = {p.position: None}
+        while p.position != terminalState:
+            p = fieldStates.get()
+
+            for k in shifts(p.position):
+                count = 0
+                if k not in closePoints:
+                    for m in range(len(k)):
+                        if k[m] != terminalState[m]:
+                            count += 1
+                    fieldStates.put(Position(k, p.start_distance + 1, p.finish_distance + count))
+                    parents[k] = p
+                    closePoints.add(k)
+        path = []
+        x = p
+        previous = p
+        while p.position != startState:
+            p = parents[p.position]
+            number = p.position[previous.position.index(0)]
+            path.append(number)
+            previous = p
+        path.reverse()
+        print(path)
+        print(x)
+
+
+startState = [3, 5, 1, 7, 2, 4, 6, 10, 13, 9, 11, 12, 8, 14, 15, 0]
+fifteenGame(startState)
